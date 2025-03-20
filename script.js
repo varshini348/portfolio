6b71764ccf2eceffe8847c694917057d7cb7fb3b
varshini348 <varshinilakshmi59@gmail.com>
2025-03-20 13:02:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.styles.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.styles.display = "none";
+  }
+
+}
+
+// Side NavIgation Menu JS Code
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.styles.opacity = "0";
+  menuBtn.styles.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.styles.opacity = "1";
+  menuBtn.styles.pointerEvents = "auto";
+  body.styles.overflow = "auto";
+  scrollBtn.styles.pointerEvents = "auto";
+}
+
+// Side Navigation Bar Close While We Click On Navigation Links
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.styles.opacity = "1";
+    menuBtn.styles.pointerEvents = "auto";
+  });
+}
